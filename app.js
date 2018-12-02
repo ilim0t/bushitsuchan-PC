5c4a44e9a80da3f684029645fa1ed14809fc1583
ilim <ilim0t14@gmail.com>
2018-12-03 00:10:31 +0900

    APIを使ってuploadする部分の作成

--- app.js
+++ app.js
@@ -3,39 +3,43 @@
 const {google} = require('googleapis');
 const path = require('path');
 const fs = require('fs');
-const readlineSync = require('readline-sync');
+const readline = require('readline');
 const request = require('request');
 
-async function example_get(oauth2Client) {
-    const accessToken = await oauth2Client.getAccessToken();
-
+async function example_upload(token, image, callback) {
     const headers = {
-        'Content-Type': 'application/application/json'
+        'Content-Type': 'application/octet-stream',
+        'X-Goog-Upload-File-Name': 'FILENAME',
+        'X-Goog-Upload-Protocol': 'raw',
     };
 
     const options = {
-        url: 'https://photoslibrary.googleapis.com/v1/albums',
-        method: 'GET',
+        url: 'https://photoslibrary.googleapis.com/v1/uploads',
+        method: 'POST',
         headers: headers,
         json: true,
         auth: {
-            bearer: accessToken.token
+            bearer: token.token
+        },
+        body: {
+            MEDIA_BINARY_DATA: image
         }
     };
 
-    request(options, (error, response, body) => {
-        console.log(body);
-    })
+    await request(options, (error, response, body) => {
+        callback(body);
+    });
 }
 
-async function main() {
+async function getToken(callback) {
     const keyPath = path.join(__dirname, 'oauth2.keys.json');
+    const TOKEN_PATH = path.join(__dirname, 'token.json');
     let keys = {};
     if (fs.existsSync(keyPath)) {
         keys = require(keyPath).web;
     }
 
-    const oauth2Client = new google.auth.OAuth2(
+    const oAuth2Client = new google.auth.OAuth2(
         keys.client_id,
         keys.client_secret,
         'urn:ietf:wg:oauth:2.0:oob'
@@ -44,18 +48,37 @@ async function main() {
     const scopes = [
         'https://www.googleapis.com/auth/photoslibrary'
     ];
-
-    const url = oauth2Client.generateAuthUrl({
+    const authUrl = oAuth2Client.generateAuthUrl({
         access_type: 'offline',
         scope: scopes
     });
-    console.log(`以下のファイルを開き，認証したあと表示される文字列をここに貼り付けてください\n${url}`);
-    const authorizationCode = readlineSync.question('入力: ');
 
-    const {tokens} = await oauth2Client.getToken(authorizationCode);
-    oauth2Client.setCredentials(tokens);
+    console.log(`以下のファイルを開き，認証したあと表示される文字列をここに貼り付けてください\n${authUrl}`);
 
-    example_get(oauth2Client);
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    await rl.question('入力: ', (authorizationCode) => {
+        rl.close();
+        oAuth2Client.getToken(authorizationCode).then(value => {
+            const token = value.tokens;
+            oAuth2Client.setCredentials(token);
+            // fs.writeFile(TOKEN_PATH, JSON.stringify(token), console.error);
+            oAuth2Client.getAccessToken().then(callback);
+        }).catch(console.error);
+    });
+}
+
+
+async function main() {
+    await getToken((token) => {
+        const image = fs.readFileSync('sample.png');
+        const base64_data = "data:image/jpeg;base64," + image.toString('base64');
+        const uploadToken = example_upload(token, base64_data, (uploadToken) => {
+            console.log('uploadToken: ', uploadToken)
+        });
+    });
 }
 
 main().catch(console.error);
