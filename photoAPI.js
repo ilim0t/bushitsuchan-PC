b27573f1be8fa27e6057178fad0e3023d2e56cf2
ilim <ilim0t14@gmail.com>
2018-12-03 22:05:49 +0900

    tokenを再利用するように変更&JSDocの追加

--- photoAPI.js
+++ photoAPI.js
@@ -6,6 +6,7 @@ const readline = require("readline");
 const path = require("path");
 const rp = require('request-promise');
 
+
 const rpap = rp.defaults({
     transform: (body, response, resolveWithFullResponse) => {
         const constentType = response.headers['content-type'].split(';')[0];
@@ -19,34 +20,44 @@ const rpap = rp.defaults({
     }
 });
 
+/**
+ @typedef {Object} OAuth2Client
+ @property {Function} getAccessToken
+ */
+
+/**
+ * @returns {Promise<OAuth2Client>}
+ */
 async function getOAuthToken() {
     const keyPath = path.join(__dirname, 'oauth2.keys.json');
     let keys = {};
     if (fs.existsSync(keyPath)) {
         keys = require(keyPath).web;
     }
-
     const oAuth2Client = new google.auth.OAuth2(
         keys["client_id"],
         keys["client_secret"],
         'urn:ietf:wg:oauth:2.0:oob'
     );
-
     const scopes = [
         'https://www.googleapis.com/auth/photoslibrary'
     ];
+
+    const tokenPath = path.join(__dirname, 'token.json');
+    if (fs.existsSync(tokenPath)) {
+        oAuth2Client.setCredentials(require(tokenPath));
+        return oAuth2Client;
+    }
     const authURL = oAuth2Client.generateAuthUrl({
         access_type: 'offline',
         scope: scopes
     });
 
-    console.log(`以下のファイルを開き，認証したあと表示される文字列をここに貼り付けてください\n${authURL}`);
-
+    console.log(`以下のサイトを開き，認証したあと表示される文字列をここに貼り付けてください\n${authURL}`);
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
     });
-
     return new Promise((resolve) => rl.question('入力: ', resolve))
         .then((authorizationCode) => {
             rl.close();
@@ -55,28 +66,41 @@ async function getOAuthToken() {
         .then(value => {
             const token = value.tokens;
             oAuth2Client.setCredentials(token);
-            return oAuth2Client.getAccessToken();
+            fs.writeFileSync(tokenPath, JSON.stringify(token));
+        }).then(() => {
+            return oAuth2Client;
         });
 }
 
-async function getAlbumList(oAuthToken) {
+/**
+ * @param {OAuth2Client} oAuth2Client
+ * @returns {Promise<Array.<Object>>}
+ */
+async function getAlbumList(oAuth2Client) {
+    const accessToken = await oAuth2Client.getAccessToken();
     const url = "https://photoslibrary.googleapis.com/v1/albums";
     const headers = {
         "Content-type": "application/json",
-        Authorization: `Bearer ${oAuthToken}`
+        Authorization: `Bearer ${accessToken.token}`
     };
-    const response = await rpap(url,
-        {
-            method: "GET",
-            headers: headers
-        });
+    const response = await rpap(url, {
+        method: "GET",
+        headers: headers
+    });
     return response["albums"]
 }
 
-async function uploadPhoto(oAuthToken, photo, filename) {
+/**
+ * @param {OAuth2Client} oAuth2Client
+ * @param {fs.ReadStream} photo
+ * @param {string} filename
+ * @returns {Promise<string>}
+ */
+async function uploadPhoto(oAuth2Client, photo, filename) {
+    const accessToken = await oAuth2Client.getAccessToken();
     const url = "https://photoslibrary.googleapis.com/v1/uploads";
     const headers = {
-        Authorization: `Bearer ${oAuthToken}`,
+        Authorization: `Bearer ${accessToken.token}`,
         "Content-type": "application/octet-stream",
         "X-Goog-Upload-File-Name": filename,
         "X-Goog-Upload-Protocol": "raw"
@@ -88,11 +112,18 @@ async function uploadPhoto(oAuthToken, photo, filename) {
     });
 }
 
-async function createMediaItem(oAuthToken, uploadToken, description) {
+/**
+ * @param {OAuth2Client} oAuth2Client
+ * @param {string} uploadToken
+ * @param {string} description
+ * @returns {Promise<Array.<Object>>}
+ */
+async function createMediaItem(oAuth2Client, uploadToken, description) {
+    const accessToken = await oAuth2Client.getAccessToken();
     const url = "https://photoslibrary.googleapis.com/v1/mediaItems:batchCreate";
     const headers = {
         "Content-type": "application/json",
-        Authorization: `Bearer ${oAuthToken}`
+        Authorization: `Bearer ${accessToken.token}`
     };
     const body = {
         "newMediaItems": [
@@ -113,13 +144,10 @@ async function createMediaItem(oAuthToken, uploadToken, description) {
 }
 
 async function main() {
-    const {token} = await getOAuthToken();
-    // const token = '';
-    console.log(token);
-    const photo = fs.createReadStream('sample.png');
-    const uploadToken = await uploadPhoto(token, photo, "sample.png");
-    const response = await createMediaItem(token, uploadToken, "discription_example");
-    console.log(response);
+    const oAuth2Client = await getOAuthToken();
+    const uploadToken = await uploadPhoto(oAuth2Client, fs.createReadStream('sample.jpeg'), "sample.jpeg");
+    const response = await createMediaItem(oAuth2Client, uploadToken, "discription_example");
+    console.log(response[0]["mediaItem"].productUrl);
 }
 
 if (require.main === module) {
