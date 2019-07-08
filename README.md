ef268e66e346d5d1d487112711fb69b7eb9f918d
ilim <ilim0t14@gmail.com>
2019-07-08 21:46:02 +0900

    add simple live server & viwer

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # bushitsuchan-PC
 OSKの部室の様子を様子をオンラインで確認できるプロジェクト 部室ちゃん.
 その部室に置いてあるPC側で動かすプログラム.
+
+## Setup
+
+### 環境変数
+NGROK_TOKENにngrokのtokenをセットする
+
+# Run
+```bash=
+node app.js
+```
+
+```bash=
+ffmpeg -re -i example.mp4 -c copy -f flv rtmp://localhost/live/bushitsuchan
+```
+
+`app.js`を実行したときのlogに
+
+```text
+Please check https://*******.ngrok.io/viewer
+```
+
+とあるのでそれを開く。
+
+> 再生が開始されないことがあるので，静止画で止まったままのときはサイトをリロードしてください。
--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 "use strict";
+
+const NodeMediaServer = require('node-media-server');
+const ngrok = require('ngrok');
+const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
+
+const nms = new NodeMediaServer({
+    rtmp: {
+        port: 1935,
+        chunk_size: 60000,
+        gop_cache: true,
+        ping: 30,
+        ping_timeout: 60
+    },
+    http: {
+        port: 8000,
+        allow_origin: '*'
+    }
+});
+nms.run();
+
+const app = express();
+app.use(logger("short"));
+app.use(cors());
+
+ngrok.authtoken(process.env.NGROK_TOKEN)
+    .then(async () => {
+        const url = await ngrok.connect(3000);
+        const liveUrl = await ngrok.connect(8000);
+
+        console.log(`Forwarding ${liveUrl} -> localhost:8000`);
+
+        app.get('/', (req, res) => res.send('Hello World!'));
+        app.get('/live', (req, res) => res.json({
+                address: `${liveUrl}/live/bushitsuchan.flv`
+            })
+        );
+        app.get('/viewer', (req, res) => res.render("flv.ejs", {url: url})
+        );
+
+        app.listen(3000, () => console.log(`Forwarding ${url} -> localhost:3000`));
+        console.log(`Please check ${url}/viewer`);
+    }).catch(e => console.error(e));
