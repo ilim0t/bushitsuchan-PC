9aeeb85dd2de7a8ebc1b03f1ca272ed20517aeb8
ilim <ilim0t14@gmail.com>
2019-07-09 13:06:02 +0900

    refactor

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # bushitsuchan-PC
-OSKの部室の様子を様子をオンラインで確認できるプロジェクト 部室ちゃん.
-その部室に置いてあるPC側で動かすプログラム.
+
+OSK の部室の様子を様子をオンラインで確認できるプロジェクト 部室ちゃん.
+その部室に置いてある PC 側で動かすプログラム.
 
 ## Setup
 
 ### 環境変数
-NGROK_TOKENにngrokのtokenをセットする
+
+NGROK_TOKEN に ngrok の token をセットする
 
 # Run
+
 ```bash=
 node app.js
 ```
@@ -16,7 +19,7 @@ node app.js
 ffmpeg -re -i example.mp4 -c copy -f flv rtmp://localhost/live/bushitsuchan
 ```
 
-`app.js`を実行したときのlogに
+`app.js`を実行したときの log に
 
 ```text
 Please check https://*******.ngrok.io/viewer
--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const NodeMediaServer = require('node-media-server');
 const ngrok = require('ngrok');
 const express = require('express');
@@ -7,39 +5,52 @@ const cors = require('cors');
 const logger = require('morgan');
 
 const nms = new NodeMediaServer({
-    rtmp: {
-        port: 1935,
-        chunk_size: 60000,
-        gop_cache: true,
-        ping: 30,
-        ping_timeout: 60
-    },
-    http: {
-        port: 8000,
-        allow_origin: '*'
-    }
+  logType: 2,
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60,
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*',
+  },
+  auth: {
+    api: false,
+    // api_user: process.env.API_USER,
+    // api_pass: process.env.API_PASS
+  },
 });
 nms.run();
+nms.on('preConnect', (id, args) => {
+  console.log('[NodeEvent on preConnect]', `id=${id} args=${JSON.stringify(args)}`);
+  if (args) {
+    const session = nms.getSession(id);
+    session.reject();
+  }
+});
 
 const app = express();
-app.use(logger("short"));
+app.use(logger('short'));
 app.use(cors());
 
-ngrok.authtoken(process.env.NGROK_TOKEN)
-    .then(async () => {
-        const url = await ngrok.connect(3000);
-        const liveUrl = await ngrok.connect(8000);
+ngrok
+  .authtoken(process.env.NGROK_TOKEN)
+  .then(async () => {
+    const url = await ngrok.connect(3000);
+    const liveUrl = await ngrok.connect(8000);
 
-        console.log(`Forwarding ${liveUrl} -> localhost:8000`);
+    console.log(`Forwarding ${liveUrl} -> localhost:8000`);
 
-        app.get('/', (req, res) => res.send('Hello World!'));
-        app.get('/live', (req, res) => res.json({
-                address: `${liveUrl}/live/bushitsuchan.flv`
-            })
-        );
-        app.get('/viewer', (req, res) => res.render("flv.ejs", {url: url})
-        );
+    app.get('/', (req, res) => res.send('Hello World!'));
+    app.get('/live', (req, res) => res.json({
+      address: `${liveUrl}/live/bushitsuchan.flv`,
+    }));
+    app.get('/viewer', (req, res) => res.render('flv.ejs', { url }));
 
-        app.listen(3000, () => console.log(`Forwarding ${url} -> localhost:3000`));
-        console.log(`Please check ${url}/viewer`);
-    }).catch(e => console.error(e));
+    app.listen(3000, () => console.log(`Forwarding ${url} -> localhost:3000`));
+    console.log(`Please check ${url}/viewer`);
+  })
+  .catch(e => console.error(e));
