df4dfa38e4ba33a466dbaf27731247ead93c30cc
ilim <ilim0t14@gmail.com>
2019-07-09 21:34:22 +0900

    use AWS API Gateway

--- README.md
+++ README.md
@@ -5,26 +5,54 @@ OSK の部室の様子を様子をオンラインで確認できるプロジェ�
 
 ## Setup
 
+### AWS CLI
+
+[AWS CLI](https://aws.amazon.com/jp/cli/)をインストール
+かつ，その設定をする
+
+```bash=
+brew install awscli
+aws configure
+```
+
+### 固定 URL 設定
+
+https://qiita.com/miso_develop/items/bdcf15489b069ba1fa61 に従い設定
+
 ### 環境変数
 
-NGROK_TOKEN に ngrok の token をセットする
+NGROK_TOKEN: ngrok の token
+AWS_REST_API_ID, RESOURCE_ID: 固定 URL 設定で得られた token
 
-# Run
+## Run
 
 ```bash=
 node app.js
 ```
 
+rtmp に向けストリーミング
+OBS などでも行えますがここでは ffmpeg の例を書きます
+
 ```bash=
 ffmpeg -re -i example.mp4 -c copy -f flv rtmp://localhost/live/bushitsuchan
 ```
 
-`app.js`を実行したときの log に
+### local での確認
 
-```text
-Please check https://*******.ngrok.io/viewer
-```
-
-とあるのでそれを開く。
+`http://localhost:3000/viewer`を開く
 
 > 再生が開始されないことがあるので，静止画で止まったままのときはサイトをリロードしてください。
+
+### remote での確認
+
+`app.js`を実行したときの log に
+
+```text=
+Remote URL: https://*****.execute-api.us-east-2.amazonaws.com/prod
+```
+
+とあるので，それを開く
+
+> 再生が開始されないことがあるので，静止画で止まったままのときはサイトをリロードしてください。
+
+> この AWS の URL は半永久的に変わりません
--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const ngrok = require('ngrok');
 const express = require('express');
 const cors = require('cors');
 const logger = require('morgan');
+const { execSync } = require('child_process');
+
+const config = {
+  restApiId: process.env.AWS_REST_API_ID,
+  resourceId: process.env.RESOURCE_ID,
+  httpMethod: 'GET',
+  region: 'us-east-2',
+};
 
 const nms = new NodeMediaServer({
   logType: 2,
@@ -24,33 +32,56 @@ const nms = new NodeMediaServer({
   },
 });
 nms.run();
-nms.on('preConnect', (id, args) => {
-  console.log('[NodeEvent on preConnect]', `id=${id} args=${JSON.stringify(args)}`);
-  if (args) {
-    const session = nms.getSession(id);
-    session.reject();
-  }
-});
+// nms.on('preConnect', (id, args) => {
+//   console.log('[NodeEvent on preConnect]', `id=${id} args=${JSON.stringify(args)}`);
+//   if (JSON.stringify(args).auth) {
+//     const session = nms.getSession(id);
+//     session.reject();
+//   }
+// });
+
+// nms.on('prePlay', (id, StreamPath, args) => {
+//   console.log(
+//     '[NodeEvent on prePlay]',
+//     `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`,
+//   );
+//   if (JSON.stringify(args).auth) {
+//     const session = nms.getSession(id);
+//     session.reject();
+//   }
+// });
 
 const app = express();
 app.use(logger('short'));
 app.use(cors());
 
-ngrok
-  .authtoken(process.env.NGROK_TOKEN)
-  .then(async () => {
-    const url = await ngrok.connect(3000);
-    const liveUrl = await ngrok.connect(8000);
+const main = async () => {
+  await ngrok.authtoken(process.env.NGROK_TOKEN);
+  const url = await ngrok.connect(3000);
+  const liveUrl = await ngrok.connect(8000);
 
-    console.log(`Forwarding ${liveUrl} -> localhost:8000`);
+  console.log(`Forwarding ${liveUrl} -> localhost:8000`);
 
-    app.get('/', (req, res) => res.send('Hello World!'));
-    app.get('/live', (req, res) => res.json({
-      address: `${liveUrl}/live/bushitsuchan.flv`,
-    }));
-    app.get('/viewer', (req, res) => res.render('flv.ejs', { url }));
+  app.get('/', (req, res) => res.send('Hello World!'));
+  app.get('/live', (req, res) => res.json({
+    address: `${liveUrl}/live/bushitsuchan.flv`,
+  }));
+  app.get('/viewer', (req, res) => res.render('flv.ejs', { url }));
 
-    app.listen(3000, () => console.log(`Forwarding ${url} -> localhost:3000`));
-    console.log(`Please check ${url}/viewer`);
-  })
-  .catch(e => console.error(e));
+  app.listen(3000, () => console.log(`Forwarding ${url} -> localhost:3000`));
+  console.log(`Please check ${url}/viewer`);
+
+  execSync(
+    `aws apigateway put-integration --rest-api-id ${config.restApiId} --resource-id ${
+      config.resourceId
+    } --http-method ${config.httpMethod} --type HTTP_PROXY --integration-http-method ${
+      config.httpMethod
+    } --uri ${url}/viewer`,
+  );
+  execSync(`aws apigateway create-deployment --rest-api-id ${config.restApiId} --stage-name prod`);
+  console.log(
+    `Remote URL: https://${config.restApiId}.execute-api.${config.region}.amazonaws.com/prod`,
+  );
+};
+
+main().catch(e => console.error(e));
