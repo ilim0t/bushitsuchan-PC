589f807a9b81d746624a21e4c264333505c2e4eb
ilim <ilim0t14@gmail.com>
2019-08-03 04:11:14 +0900

    Weight reduction of image & add express container

--- broadcast-server/broadcast.sh
+++ broadcast-server/broadcast.sh
@@ -9,6 +9,6 @@ ffmpeg \
     -preset veryfast \
     -tune zerolatency,stillimage,film \
     -vb 2500k \
-    -vf "drawtext=fontfile='/usr/share/fonts/opentype/ipafont-gothic/ipag.ttf':text='%{localtime}':fontcolor=white@0.7:bordercolor=black@0.7:borderw=3:x=0:y=h-lh*1.2:fontsize=24" \
+    -vf "drawtext=fontfile='/ipag00303/ipag.ttf':text='%{localtime}':fontcolor=white@0.7:bordercolor=black@0.7:borderw=3:x=0:y=h-lh*1.2:fontsize=24" \
     -f flv "${RTMP_SERVER_URL}/${STREAM_NAME}" \
     -loglevel warning
