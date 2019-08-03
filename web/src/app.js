7c51c3d798171111c3629e7246e1f075f9512961
ilim <ilim0t14@gmail.com>
2019-08-04 01:51:58 +0900

    Rewrote for docker

--- web/src/app.js
+++ web/src/app.js
@@ -1,90 +1,229 @@
-const RtmpServer = require('./rtmp-server');
-const ngrok = require('./ngrok');
-const Server = require('./server');
-const aws = require('./aws');
-const Stream = require('./stream');
-const { daemon } = require('./utils');
-const slack = require('./slack');
+const url = require('url');
+const axios = require('axios');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const fs = require('fs');
+const assert = require('assert');
+const querystring = require('querystring');
+const proxy = require('proxy-middleware');
 
-const config = {
-  restApiId: process.env.AWS_REST_API_ID,
-  viewerResourceId: process.env.VIEWER_RESOURCE_ID,
-  oauthResourceId: process.env.OAUTH_RESOURCE_ID,
-  httpMethod: 'GET',
-  ngrokToken: process.env.NGROK_TOKEN,
-  slackClientId: process.env.SLACK_CLIENT_ID,
-  slackClientSecret: process.env.SLACK_CLIENT_SECRET,
-  slackBotAccessToken: process.env.SLACK_BOT_ACCESS_TOKEN,
-  slackSigningSecret: process.env.SLACK_SIGNING_SECRET,
-  contactChannel: process.env.CONTACT_CHANNEL,
-  wsId: process.env.WORKSTATION_ID,
-  privateKey: process.env.PRIVATE_KEY,
-  debug: process.env.DEBUG && Boolean(JSON.parse(process.env.DEBUG)),
-  isMac: process.env.IS_MAC && Boolean(JSON.parse(process.env.IS_MAC)),
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+
+const sessionSecret = process.env.SESSION_SECRET;
+const slackClientId = process.env.SLACK_CLIENT_ID;
+const slackClientSecret = process.env.SLACK_CLIENT_SECRET;
+const wsId = process.env.WORKSTATION_ID;
+
+const getToken = async (code, clientId, clientSecret) => {
+  assert(code !== undefined);
+  const tokenResponse = await axios.post(
+    `https://slack.com//api/oauth.access?${querystring.stringify({
+      client_id: clientId,
+      client_secret: clientSecret,
+      code,
+    })}`, // TODO header: application/json
+  ); // TODO cactch
+  const { data } = tokenResponse;
+  assert(data !== undefined);
+
+  // The code passed is incorrect or expired
+  if (!data.ok) {
+    throw TypeError(`codeが無効値${code}で${tokenResponse.data.error}`);
+  }
+  return data.access_token;
 };
 
-const liveServer = new RtmpServer(1935);
-liveServer.run();
-console.log(`RTMP server listening on port ${1935}`);
+const authorize = async (token, workstationId) => {
+  assert(token !== undefined);
 
-const disk = new Stream(
-  config.isMac ? `${__dirname}/hls` : '/dev/shm',
-  'bushitsuchan',
-  config.isMac,
+  const response = await axios.get(
+    `https://slack.com/api/users.identity?${querystring.stringify({ token })}`,
+  ); // TODO header: application/json, catch
+
+  const { data } = response;
+  assert(data !== undefined);
+
+  if (!data.ok) {
+    throw new TypeError();
+  }
+  if (data.team.id !== workstationId) {
+    throw new Error();
+  }
+  return data.user.name;
+};
+
+// const resPhoto = (rtmpAddress, ext = "jpg") => (req, res) => {
+//   const ffmpeg = childProcess.spawn("ffmpeg", [
+//     "-i",
+//     `${rtmpAddress}`,
+//     "-ss",
+//     "0.7",
+//     "-vframes",
+//     "1",
+//     "-f",
+//     "image2",
+//     "pipe:1"
+//   ]);
+
+//   res.contentType(`image/${ext}`);
+//   ffmpeg.stdout.pipe(res);
+// };
+
+const app = express();
+
+app.set('trust proxy', 1);
+app.use(helmet());
+app.use(cors());
+// app.use(
+//   cookieSession({
+//     secret: this.config.privateKey,
+//     maxAge: 1000 * 60 * 60 * 24 * 15, // 有効期限15日
+//     secure: true,
+//     httpOnly: true
+//   })
+// );
+
+app.use(
+  session({
+    // store: new RedisStore({
+    //   host: 'redis',
+    //   port: 6379,
+    //   //   prefix: "dev.web"
+    // }),
+    secret: sessionSecret,
+    resave: false,
+    cookie: { secure: true },
+  }),
 );
-console.log(`Regarding directory ${disk.mountPath} as RAM DISK`);
 
-disk
-  .run(config.isMac)
-  .then(async (mountPath) => {
-    console.log(`Please put HLS files in ${mountPath}`);
-    let input;
-    if (config.isMac) {
-      input = 'ffmpeg -f avfoundation -re -i 0 -r 10';
-    } else {
-      input = 'ffmpeg -s 1280x960 -i /dev/video0';
-    }
-    daemon(
-      `${input} -vcodec libx264 -pix_fmt yuv420p -preset veryfast -tune zerolatency,stillimage,film -vb 2500k -vf "drawtext=text='%{localtime}':fontcolor=white@0.7:bordercolor=black@0.7:borderw=3:x=0:y=h-lh*1.2:fontsize=24" -f flv rtmp://localhost:${1935}/live/bushitsuchan -loglevel warning`,
-    );
-    daemon(
-      `ffmpeg -i rtmp://localhost:1935/live/bushitsuchan -hls_flags delete_segments -g 40 -f hls ${mountPath}/output.m3u8 -loglevel warning`,
-    );
+app.use(
+  morgan('<@:user> [:date[clf]] :method :url :status :res[content-length] - :response-time ms', {
+    skip: (req, res) => ['.ts', '.m3u8', '.jpg'].some(element => req.path.endsWith(element)),
+  }),
+);
 
-    let ngrokUrl;
-    let awsUrl;
+morgan.token('user', (req, res) => req.session && (req.session.name || 'anonymous'));
+// morgan.token("date", () => new Date().toLocaleString());
 
-    if (config.debug) {
-      ngrokUrl = null;
-      awsUrl = null;
-    } else {
-      ngrokUrl = await ngrok.run(config.ngrokToken, 3000);
-      console.log(`Forwarding ${ngrokUrl} -> localhost:${3000}`);
+// app.use(
+//   morgan(
+//     ':remote-addr - :remote-user <@:user> [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
+//     {
+//       stream: fs.createWriteStream(`${__dirname}/log/access.log`, {
+//         flags: "a"
+//       })
+//     }
+//   )
+// );
 
-      awsUrl = await aws.run(config, ngrokUrl);
-      console.log(`Remote URL: ${awsUrl}`);
-    }
+app.get('/', (req, res) => res.send('Hello Bushitsuchan!'));
 
-    const server = new Server(
-      ngrokUrl,
-      awsUrl,
-      mountPath,
-      config,
-      `rtmp://localhost:1935/live/${'bushitsuchan'}`,
-    );
-    server.app.use(
-      '/slack',
-      slack(
-        awsUrl,
-        config.contactChannel,
-        `rtmp://localhost:${1935}/live/bushitsuchan`,
-        config.slackBotAccessToken,
-        config.slackSigningSecret,
-      ),
-    );
-    server.run(3000).then(() => console.log(`Express app listening on port ${3000}`));
-  })
-  .catch((e) => {
-    console.error(e);
-    disk.close();
-  });
+app.get('/login', (req, res) => {
+  const scopes = ['identity.basic'];
+  res.redirect(
+    `https://slack.com/oauth/authorize?${querystring.stringify({
+      client_id: slackClientId,
+      scope: scopes.join(' '),
+      team: wsId, // うまく機能しない
+    })}`,
+  );
+});
+
+app.get('/oauth-redirect', (req, res) => {
+  const { code } = req.query;
+  if (code === undefined) {
+    // TODO
+    return;
+  }
+  getToken(code, slackClientId, slackClientSecret)
+    .then((token) => {
+      req.session.token = token;
+      res.redirect('viewer');
+    })
+    .catch(e => console.error(e));
+});
+
+app.get('/logout', (req, res) => {
+  // TODO When not logged in
+  req.session.destroy();
+  res.send('You have successfully logged out');
+});
+
+app.use(['/viewer', '/photo-viewer'], (req, res, next) => {
+  const { token } = req.session;
+  if (token === undefined) {
+    // TODO debug mode
+    res.redirect('login');
+    return;
+  }
+  next();
+});
+
+app.use(['/auth', '/data'], (req, res, next) => {
+  const { token } = req.session;
+  if (token === undefined) {
+    // TODO debug mode
+    res.sendStatus(401).end();
+    return;
+  }
+  next();
+});
+
+app.get('/auth', (req, res) => {
+  // TODO debug mode
+  authorize(req.session.token, wsId)
+    .then((name) => {
+      req.session.name = name;
+      req.session.lastAuthedTime = Date.now();
+      res.json({
+        hlsAddress: ' data/output.m3u8',
+        photoAddress: 'data/photo.jpg',
+      });
+    })
+    .catch((e) => {
+      if (e instanceof TypeError) {
+        res.sendStatus(401).end(); // 再login
+      } else {
+        res.sendStatus(403).end(); // 権限をもっていない
+      }
+    });
+});
+
+app.get(['/viewer', '/photo-viewer'], (req, res) => {
+  res.sendFile(`./views/${req.url}.html`, { root: __dirname });
+});
+
+const expireTime = 60 * 1000;
+
+app.use('/data', (req, res, next) => {
+  const { token, lastAuthedTime } = req.session;
+  assert(lastAuthedTime !== undefined);
+
+  if (lastAuthedTime + expireTime > Date.now()) {
+    next();
+    return;
+  }
+  authorize(token, wsId)
+    .then((name) => {
+      req.session.name = name;
+      req.session.lastAuthedTime = Date.now();
+      next();
+    })
+    .catch(() => res.send(403).end());
+});
+
+// this.app.use("/data", (req, res, next) => {
+//   const { lastAuthedTime } = req.session;
+//   if (lastAuthedTime > Date.now()) {
+//     req.session = null;
+//     res.sendStatus(403).end();
+//   }
+//   next();
+// });
+
+app.use('/data', proxy(url.parse('http://data-server/')));
+
+app.listen(80, () => console.log('Express app listening on port 80.'));
--- web/src/ngrok.js
+++
@@ -1,7 +0,0 @@
-const ngrok = require('ngrok');
-
-module.exports.run = async (token, port = 3000) => {
-  await ngrok.authtoken(token);
-  const url = await ngrok.connect({ addr: port, region: 'ap' });
-  return url;
-};
