ed72d4d2b612ab92f85df2a48721681cf873a5eb
ilim <ilim0t14@gmail.com>
2019-10-07 02:28:30 +0900

    Update preview

--- object-detection/src/app.py
+++ object-detection/src/app.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
 import glob
-import io
-import json
 import os
-import time
+from io import BytesIO
+from time import time
 from typing import Dict
 
 import numpy as np
@@ -12,19 +11,17 @@ import requests
 from flask import Flask, jsonify, request
 from PIL import Image
 
+from draw import LABEL_NAME, draw
 from model import FasterRCNNResnet101Coco
 
 image_dir = "/dev/shm/photo"
+os.makedirs(image_dir, exist_ok=True)
+
 app = Flask(__name__, static_url_path='/photo', static_folder=image_dir)
 
 
 app.logger.info("Loading network")
 net = FasterRCNNResnet101Coco(os.getenv("DEVICE", "CPU"), os.getenv("CPU_EXTENSION"))
-app.logger.info("Network loading finished")
-
-with open("labels.json") as f:
-    label_names = json.load(f)
-os.makedirs(image_dir, exist_ok=True)
 
 
 @app.route('/faster_rcnn_resnet101_coco')
@@ -35,7 +32,7 @@ def object_detection():
 
     url = f"http://media/photo/{photo_id}"
     image = requests.get(url)
-    image = Image.open(io.BytesIO(image.content)).convert('RGB')
+    image = Image.open(BytesIO(image.content)).convert('RGB')
     image = np.asarray(image)
 
     output = net(image)
@@ -43,21 +40,17 @@ def object_detection():
 
     image = draw(image, output)
     for f in glob.glob(f"{image_dir}/*"):
-        if f.split("/")[-1].isdecimal() and float(f.split("/")[-1]) / 1000 + retention < time.time():
+        if f.split("/")[-1].isdecimal() and float(f.split("/")[-1]) / 1000 + retention < time():
             os.remove(f)
     Image.fromarray(image).save(f"{image_dir}/{photo_id}", "JPEG")
 
     return jsonify({
-        "bbox": (output["bbox"].reshape(-1, 2, 2) * np.array(image.shape[:2])).reshape(-1, 4).astype(int).tolist(),
+        "bbox": (output["bbox"].reshape(-1, 2, 2) * np.array(image.shape[-2::-1])).reshape(-1, 4).astype(int).tolist(),
         "confidence": output["conf"].tolist(),
         "label_id": output["label"].astype(int).tolist(),
-        "label_name": [label_names[label] for label in output["label"].astype(int)],
+        "label_name": [LABEL_NAME[label] for label in output["label"].astype(int)],
         "photo_id": photo_id
     })
-
-
-def draw(image: np.ndarray, prediction: Dict[str, np.ndarray]) -> np.ndarray:
-    return image
 
 
 def select_top_prediction(prediction: Dict[str, np.ndarray], threshold: float) -> Dict[str, np.ndarray]:
