38235b212354eb7113361217d1dd5ce0218ac380
ilim <ilim0t14@gmail.com>
2019-11-05 02:02:52 +0900

    Install only necessary OpenVINO components

--- object-detection/src/get_model.sh
+++ object-detection/src/get_model.sh
@@ -1,9 +1,9 @@
-python /opt/intel/openvino/deployment_tools/tools/model_downloader/downloader.py \
+python /opt/intel/openvino/deployment_tools/open_model_zoo/tools/downloader/downloader.py \
     --name $1 \
     --cache_dir ./.cache \
     --output_dir ./models
 
-python /opt/intel/openvino/deployment_tools/tools/model_downloader/converter.py \
+python /opt/intel/openvino/deployment_tools/open_model_zoo/tools/downloader/converter.py \
     --name $1 \
     --precisions=FP16 \
     --mo /opt/intel/openvino/deployment_tools/model_optimizer/mo.py \
