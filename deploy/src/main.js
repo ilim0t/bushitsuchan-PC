9c61a4b93f72ce85533497398fe79a4f3b698eb0
ilim <ilim0t14@gmail.com>
2019-11-19 23:05:25 +0900

    Fix path & verify signature

--- deploy/src/main.js
+++ deploy/src/main.js
@@ -1,7 +1,31 @@
 const express = require('express');
+const crypto = require('crypto');
+
+const { DEPLOY_SECRET } = process.env;
+
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/', (req, res) => {
+  const signature = req.headers['x-hub-signature'];
+
+  const shasum = crypto.createHmac('sha1', DEPLOY_SECRET);
+  shasum.update(JSON.stringify(req.body));
+  const hash = `sha1=${shasum.digest('hex')}`;
+
+  if (signature === undefined) {
+    res.sendStatus(401);
+    return;
+  }
+  if (signature !== hash) {
+    res.sendStatus(403);
+    return;
+  }
+  res.send('Hello World!');
+});
+
 
 app.listen(80, () => console.log('Example app listening on port 80!'));
