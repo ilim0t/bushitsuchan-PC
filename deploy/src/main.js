933e237789432aea59d1babad1cd34e88f7389e3
ilim <ilim0t14@gmail.com>
2019-12-17 16:18:53 +0900

    Add docker-compose command & Fix bug

--- deploy/src/main.js
+++ deploy/src/main.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const crypto = require('crypto');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const childProcess = require('child_process');
+const util = require('util');
 
-const { DEPLOY_SECRET } = process.env;
+const execFile = util.promisify(childProcess.execFile);
 
+const { DEPLOY_SECRET, HOOK_BRANCH, DEPLOYMENT_ENV } = process.env;
 
 const app = express();
 
+app.set('trust proxy', 1);
+app.use(helmet());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan('short'));
 
 app.post('/', (req, res) => {
   const signature = req.headers['x-hub-signature'];
@@ -24,8 +34,17 @@ app.post('/', (req, res) => {
     res.sendStatus(403);
     return;
   }
-  res.send('Hello World!');
+  if (req.body.ref !== `refs/heads/${HOOK_BRANCH}`) {
+    req.sendStatus(202);
+  }
+
+  execFile('docker-compose', ['-f', 'utils/docker-compose.deploy.yml', 'up', '-d', '--force-recreate', DEPLOYMENT_ENV], { cwd: '/bushitsuchan-PC' });
+  res.sendStatus(200);
 });
 
+app.get('/', (req, res) => {
+  execFile('docker-compose', ['-f', 'utils/docker-compose.deploy.yml', 'up', '-d', '--force-recreate', DEPLOYMENT_ENV], { cwd: '/bushitsuchan-PC' });
+  res.sendStatus(200);
+});
 
-app.listen(80, () => console.log('Example app listening on port 80!'));
+app.listen(80, () => console.log('Example app listening on port 80.'));
